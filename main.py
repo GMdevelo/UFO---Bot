435289c9bb7ed3c341ac67eb92d71209fa926c83
GMdevelo <>
2021-05-11 18:00:19 +0000

    Help page & gif

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import discord 
+from discord.ext import commands,tasks
 import asyncio
 import os
 from keep_alive import keep_alive
+import youtube_dl
+from dotenv import load_dotenv
+
 
 client = discord.Client()
 
@@ -22,12 +26,40 @@ async def on_message(message):
   if message.author == client.user:
     return
   
+
+    """
+  if message.content.is('ufo'):
+    await message.channel.send('DU WAGST ES UNS ZU ERWÄHNEN???')
+
+  if message.content.is('UFO'):
+    await message.channel.send('DU WAGST ES UNS ZU ERWÄHNEN???')
+  
+  """
+  if message.content.startswith('?ufo help'):
+    embed = discord.Embed(title='UFO BOT',
+    description = 'all comands -- prefix is   ?ufo \r\n',
+    color=0x9AFF0A,)
+
+    
+    embed.add_field(name='?ufo help', value='shows this help page', inline = False)
+    embed.add_field(name='?ufo future', value='predicts the future', inline = False)
+    embed.add_field(name='?ufo status', value='shows current bot status', inline = False)
+
+    embed.set_footer(text='You dont have to find out more about us...')
+
+    await message.channel.send(embed=embed)
+
+
+  if message.content.startswith('?ufo status'):
+    await message.channel.send('collecting information about humans...  ')
+    await message.channel.send('https://cdn.dribbble.com/users/59100/screenshots/1869567/ufo.gif')
+
   if message.content.startswith('?ufo future'):
     await message.channel.send('WE WILL RULE THE WORLD')
 
-  if message.content.startswith('?ufo help'):
-    embed: discord.Embed(title='UFO BOT')
-    await message.channel.send('**UFO BOT**\r\n ```?ufo help -- zeigt diese Hilfe```')
+  if message.content.startswith('?ufo info'):
+    await message.channel.send('These information we have collected about you:')
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
